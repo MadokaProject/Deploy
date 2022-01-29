baf8017c1216ff28542380420e41e246ee927760
Colsrch <colsrch@foxmail.com>
2022-01-29 15:30:22 +0800

    udpte

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ chmod +x ./mcl/mcl
 wget https://github.com/iTXTech/mcl-addon/releases/download/v$mcl_releases/mcl-addon-$mcl_releases.jar -P ./mcl/plugins
 wget https://github.com/project-mirai/mirai-api-http/releases/download/$mah_releases/mirai-api-http-$mah_releases.mirai.jar -P ./mcl/plugins
 cd ./mcl && ./mcl &
-sleep 30
+sleep 60
 kill %1
