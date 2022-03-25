7affe1860be1c13c52c887f922f2561ade2851ab
Colsrch <colsrch@foxmail.com>
2022-03-25 19:48:33 +0800

    update mcl version

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mcl_releases="1.2.2"
-mah_releases="v2.4.0"
+mah_releases="v2.5.0"
 wget https://github.com/iTXTech/mirai-console-loader/releases/download/v$mcl_releases/mcl-$mcl_releases.zip
 unzip mcl-$mcl_releases.zip -d ./mcl
 chmod +x ./mcl/mcl
