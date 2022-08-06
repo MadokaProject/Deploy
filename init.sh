6e14b59b62743e89938b5dcb2f7a4797c065bacf
Colsrch <colsrch@foxmail.com>
2022-08-06 10:46:28 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ unzip mcl-$mcl_releases.zip -d ./mcl
 chmod +x ./mcl/mcl
 wget https://github.com/project-mirai/mirai-api-http/releases/download/$mah_releases/mirai-api-http-$mah_releases.mirai2.jar -P ./mcl/plugins
 cd ./mcl && ./mcl &
-sleep 60
+sleep 120
 kill %1
