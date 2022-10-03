782dc02937b669e26273ca8d75802f5673d0212d
Colsrch <colsrch@foxmail.com>
2022-10-03 21:21:49 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mcl_releases="1.2.3"
-mah_releases="2.5.1"
+mcl_releases="2.1.1"
+mah_releases="2.6.2"
 wget https://github.com/iTXTech/mirai-console-loader/releases/download/v$mcl_releases/mcl-$mcl_releases.zip
 unzip mcl-$mcl_releases.zip -d ./mcl
 chmod +x ./mcl/mcl
-wget https://github.com/project-mirai/mirai-api-http/releases/download/v$mah_releases/mirai-api-http-$mah_releases.mirai.jar -P ./mcl/plugins
+wget https://github.com/project-mirai/mirai-api-http/releases/download/v$mah_releases/mirai-api-http-$mah_releases.mirai2.jar -P ./mcl/plugins
 cd ./mcl && ./mcl &
 sleep 120
 kill %1
