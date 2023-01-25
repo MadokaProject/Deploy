d562db14185d0ff7488bf066cb45e11d74b7ae82
Colsrch <colsrch@foxmail.com>
2023-01-25 17:12:42 +0800

    upgrade mah 2.7.1

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mcl_releases="2.1.2"
-mah_releases="2.6.2"
+mah_releases="2.7.1-publish1"
 wget https://github.com/iTXTech/mirai-console-loader/releases/download/v$mcl_releases/mcl-$mcl_releases.zip
 unzip mcl-$mcl_releases.zip -d ./mcl
 chmod +x ./mcl/mcl
